86ea5813bdc2e182c50cf66feef1173c0c18fa33
bedrin <Dmitry.Bedrin@gmail.com>
2017-08-19 21:53:26 +0300

    Move SunJaasKrb5LoginConfig to com.kerb4j.jaas.sun

--- src/main/java/com/kerb4j/jaas/sun/SunJaasKrb5LoginConfig.java
+++ src/main/java/com/kerb4j/jaas/sun/SunJaasKrb5LoginConfig.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.kerb4j.jaas;
+package com.kerb4j.jaas.sun;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
