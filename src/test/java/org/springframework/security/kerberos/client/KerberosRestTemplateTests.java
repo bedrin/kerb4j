70e8ad8a5a832edecd0fbc872ab9e8229ce540a4
bedrin <Dmitry.Bedrin@gmail.com>
2017-08-20 14:37:49 +0300

    Removed ApacheDS logging; fixed testSpnegoWithForward()

--- src/test/java/org/springframework/security/kerberos/client/KerberosRestTemplateTests.java
+++ src/test/java/org/springframework/security/kerberos/client/KerberosRestTemplateTests.java
@@ -15,25 +15,6 @@
  */
 package org.springframework.security.kerberos.client;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-import java.net.HttpURLConnection;
-import java.net.InetAddress;
-import java.net.URL;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-
 import com.kerb4j.client.SpnegoClient;
 import com.kerb4j.client.SpnegoHttpURLConnection;
 import org.junit.After;
@@ -41,12 +22,7 @@ import org.junit.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.PropertyPlaceholderAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.HttpMessageConvertersAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.ServerPropertiesAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.*;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.ApplicationListener;
@@ -54,16 +30,26 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.kerberos.test.KerberosSecurityTestcase;
 import org.springframework.security.kerberos.test.MiniKdc;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.DefaultResponseErrorHandler;
-import org.springframework.web.client.RestTemplate;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.lang.annotation.*;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.URL;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class KerberosRestTemplateTests extends KerberosSecurityTestcase {
 
@@ -78,42 +64,12 @@ public class KerberosRestTemplateTests extends KerberosSecurityTestcase {
 	}
 
     @Test
-    public void testSpnego() throws Exception {
-
-		MiniKdc kdc = getKdc();
-		File workDir = getWorkDir();
-		String host = InetAddress.getLocalHost().getCanonicalHostName();
-
-		String serverPrincipal = "HTTP/" + host;
-		File serverKeytab = new File(workDir, "server.keytab");
-		kdc.createPrincipal(serverKeytab, serverPrincipal);
-
-		String clientPrincipal = "client/" + host;
-		File clientKeytab = new File(workDir, "client.keytab");
-		kdc.createPrincipal(clientKeytab, clientPrincipal);
-
-
-		context = SpringApplication.run(new Object[] { WebSecurityConfig.class, VanillaWebConfiguration.class,
-				WebConfiguration.class }, new String[] { "--security.basic.enabled=true",
-				"--security.user.name=username", "--security.user.password=password",
-				"--serverPrincipal=" + serverPrincipal, "--serverKeytab=" + serverKeytab.getAbsolutePath() });
-
-		PortInitListener portInitListener = context.getBean(PortInitListener.class);
-		assertThat(portInitListener.latch.await(10, TimeUnit.SECONDS), is(true));
-		int port = portInitListener.port;
-
-		/*KerberosRestTemplate restTemplate = new KerberosRestTemplate(clientKeytab.getAbsolutePath(), clientPrincipal);
-
-		String response = restTemplate.getForObject("http://" + host + ":" + port + "/hello", String.class);
-		assertThat(response, is("home"));*/
-    }
-
-    @Test
     public void testSpnegoWithForward() throws Exception {
 
 		MiniKdc kdc = getKdc();
 		File workDir = getWorkDir();
-		String host = InetAddress.getLocalHost().getCanonicalHostName();
+		String host = InetAddress.getLocalHost().getCanonicalHostName().toLowerCase(); // doesn't work without toLowerCse
+
 
 		String serverPrincipal = "HTTP/" + host;
 		File serverKeytab = new File(workDir, "server.keytab");
@@ -131,17 +87,14 @@ public class KerberosRestTemplateTests extends KerberosSecurityTestcase {
 		// TODO: should tweak minikdc so that we can use kerberos principals
 		//       which are not valid, for now just use plain RestTemplate
 
-		// just checking that we get 401 which we skip and
-		// get login page content
-		/*RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory());
-		restTemplate.setErrorHandler(new DefaultResponseErrorHandler() {
-			@Override
-			public void handleError(ClientHttpResponse response) throws IOException {
-			}
-		});
+		HttpURLConnection huc = (HttpURLConnection)
+				new URL("http://" + host + ":" + port + "/hello").openConnection();
 
-		String response = restTemplate.getForObject("http://" + host + ":" + port + "/hello", String.class);
-		assertThat(response, is("login"));*/
+		assertEquals(401, huc.getResponseCode());
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(huc.getErrorStream()));
+		assertEquals("login", br.readLine());
+
     }
 
     @Test
@@ -250,7 +203,7 @@ public class KerberosRestTemplateTests extends KerberosSecurityTestcase {
                     DispatcherServletAutoConfiguration.class, WebMvcAutoConfiguration.class,
                     HttpMessageConvertersAutoConfiguration.class,
                     ErrorMvcAutoConfiguration.class, PropertyPlaceholderAutoConfiguration.class })
-    protected static @interface MinimalWebConfiguration {
+    protected @interface MinimalWebConfiguration {
     }
 
 }
