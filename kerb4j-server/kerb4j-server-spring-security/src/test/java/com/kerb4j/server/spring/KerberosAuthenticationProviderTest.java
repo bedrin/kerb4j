c2c739ef19cec7384eeb91a46a0bb920fa1e48fc
bedrin <Dmitry.Bedrin@gmail.com>
2018-02-02 23:13:22 +0300

    issues/17 Support JDK 1.9 - base64 compatibility

--- kerb4j-server/kerb4j-server-spring-security/src/test/java/com/kerb4j/server/spring/KerberosAuthenticationProviderTest.java
+++ kerb4j-server/kerb4j-server-spring-security/src/test/java/com/kerb4j/server/spring/KerberosAuthenticationProviderTest.java
@@ -15,15 +15,11 @@
  */
 package com.kerb4j.server.spring;
 
-import com.kerb4j.client.SpnegoClient;
+import com.kerb4j.KerberosSecurityTestcase;
+import com.kerb4j.MiniKdc;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.rule.PowerMockRule;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -34,11 +30,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 
 import javax.security.auth.login.LoginException;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Test class for {@link com.kerb4j.server.spring.KerberosAuthenticationProvider}
@@ -46,11 +42,7 @@ import static org.mockito.Mockito.*;
  * @author Mike Wiesner
  * @since 1.0
  */
-@PrepareForTest(SpnegoClient.class)
-public class KerberosAuthenticationProviderTest {
-
-    @Rule
-    public PowerMockRule rule = new PowerMockRule();
+public class KerberosAuthenticationProviderTest extends KerberosSecurityTestcase {
 
     private com.kerb4j.server.spring.KerberosAuthenticationProvider provider;
     private UserDetailsService userDetailsService;
@@ -59,12 +51,11 @@ public class KerberosAuthenticationProviderTest {
     private static final String TEST_PASSWORD = "password";
     private static final UsernamePasswordAuthenticationToken INPUT_TOKEN = new UsernamePasswordAuthenticationToken(TEST_USER, TEST_PASSWORD);
     private static final List<GrantedAuthority> AUTHORITY_LIST = AuthorityUtils.createAuthorityList("ROLE_ADMIN");
-    private static final UserDetails USER_DETAILS = new User(TEST_USER, "empty", true, true, true,true, AUTHORITY_LIST);
+    private static final UserDetails USER_DETAILS = new User(TEST_USER, TEST_PASSWORD, true, true, true, true, AUTHORITY_LIST);
 
     @Before
     public void before() throws LoginException {
         // mocking
-        PowerMockito.mockStatic(SpnegoClient.class);
 
         this.userDetailsService = mock(UserDetailsService.class);
         this.provider = new KerberosAuthenticationProvider();
@@ -73,21 +64,13 @@ public class KerberosAuthenticationProviderTest {
 
     @Test
     public void testLoginOk() throws Exception {
-        final AtomicInteger counter = new AtomicInteger();
+
+        MiniKdc kdc = getKdc();
+        kdc.createPrincipal(TEST_USER, TEST_PASSWORD);
 
         when(userDetailsService.loadUserByUsername(TEST_USER)).thenReturn(USER_DETAILS);
-        when(SpnegoClient.loginWithUsernamePassword(anyString(), anyString()))
-                .then(new Answer<Object>() {
-                    @Override
-                    public Object answer(InvocationOnMock invocationOnMock) throws Throwable {
-                        counter.incrementAndGet();
-                        return null;
-                    }
-                });
 
         Authentication authenticate = provider.authenticate(INPUT_TOKEN);
-
-        assertEquals(1, counter.get());
 
         assertNotNull(authenticate);
         assertEquals(TEST_USER, authenticate.getName());
